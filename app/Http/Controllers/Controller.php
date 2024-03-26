12a77f1f226a63f4dc4ac225434042b5203ca3b8
Davit <haxverdyan.2000@gmail.com>
2024-03-26 18:28:48 +0400

    all

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Service;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    public $service;
     use AuthorizesRequests, ValidatesRequests;
+    public function __construct(Service $service)
+    {
+        $this->service = $service;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\DeskController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('users', [AuthController::class, 'index']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::apiResources([
+    'desks' => DeskController::class,
+]);
+});
+Route::POST('/login', [AuthController::class, 'login']);
